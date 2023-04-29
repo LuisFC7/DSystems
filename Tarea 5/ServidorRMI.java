2ae0ea18b6c7188b07fdefd34058621c205e218e
Luis Antonio Flores Castro <55300309+LuisFC7@users.noreply.github.com>
2023-04-29 17:22:54 -0600

    Add files via upload

---
+++ Tarea 5/ServidorRMI.java
@@ -0,0 +1,11 @@
+import java.rmi.Naming;
+
+public class ServidorRMI {
+    public static void main(String[] args) throws Exception {
+        String url = "rmi://localhost/ServidorRMI";
+        ClaseRMI obj = new ClaseRMI();
+
+        // Registra la instancia en el rmiregistry
+        Naming.rebind(url, obj);
+    }
+}
